370a418ea4155edaf1c8ad4969205c0d0407a748
Talha <talha@fomarkmedia.com>
2019-10-30 12:44:20 +0500

    Initial Commit

---
+++ Custom_Card/Custom_Card_View.swift
@@ -0,0 +1,31 @@
+//
+//  Custom_Card_View.swift
+//  Custom_Card
+//
+//  Created by Apple on 30/10/2019.
+//  Copyright © 2019 Talha. All rights reserved.
+//
+
+import UIKit
+
+class Custom_Card_View: UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat = 2
+    
+    @IBInspectable var shadowOffsetWidth: Int = 0
+    @IBInspectable var shadowOffsetHeight: Int = 3
+    @IBInspectable var shadowColor: UIColor? = UIColor.black
+    @IBInspectable var shadowOpacity: Float = 0.5
+    
+    override func layoutSubviews() {
+        layer.cornerRadius = cornerRadius
+        let shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
+        
+        layer.masksToBounds = false
+        layer.shadowColor = shadowColor?.cgColor
+        layer.shadowOffset = CGSize(width: shadowOffsetWidth, height: shadowOffsetHeight);
+        layer.shadowOpacity = shadowOpacity
+        layer.shadowPath = shadowPath.cgPath
+    }
+    
+}
---
+++ Custom_Card/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Custom_Card
+//
+//  Created by Apple on 30/10/2019.
+//  Copyright © 2019 Talha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
